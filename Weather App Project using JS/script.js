0b899dcc9e3f84e67c9625b7ebf3cd643d1dd0e9
Bharath M <159880744+Bharath-M30@users.noreply.github.com>
2024-08-05 08:22:32 +0530

    Update script.js

--- Weather App Project using JS/script.js
+++ Weather App Project using JS/script.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "a2b52051b1122e9485fb573dd3cf0cea";
+const apiKey = //"apikey";
 
 document.addEventListener("submit", async event => {
 
